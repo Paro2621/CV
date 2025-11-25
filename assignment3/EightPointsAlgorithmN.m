e3d109b72b26e5195b44a6e010287ddda1ef4558
ric <riccardoparotelli@gmail.com>
2025-11-25 09:38:41 +0100

    minor changes

--- assignment3/EightPointsAlgorithmN.m
+++ assignment3/EightPointsAlgorithmN.m
@@ -1,29 +1,34 @@
 function F = EightPointsAlgorithmN(P1, P2)
+    % task 1.2.1 - normalization
     [newpts1, T1] = normalise2dpts(P1);
     [newpts2, T2] = normalise2dpts(P2);
     
+    % task 1.2.2 - EightPointsAlgorithm 
     n = size(P1,2);
 
     m2 = [1 0 0; 1 0 0; 1 0 0; 0 1 0; 0 1 0; 0 1 0; 0 0 1; 0 0 1; 0 0 1];
     m1 = [1 0 0; 0 1 0; 0 0 1; 1 0 0; 0 1 0; 0 0 1; 1 0 0; 0 1 0; 0 0 1];
-
+    
     A = zeros(n,9);
-
+    
+    % task 1.1.1 - write matrix A
     for i = 1:n        
         A(i, :) = (m2*newpts2(:, i))' .* (m1*newpts1(:, i))' ;
     end
 
+    % task 1.1.2 - SVD decomposition and solution selection
     [~, ~, V] = svd(A);
     
     V_end = V(:, end);
     
     F = [V_end(1:3)'; V_end(4:6)'; V_end(7:9)'];
     
-    % since rank(F) == 3 we anforce rank 2
+    % task 1.1.3 - rank 2 enforcement and F recomputation
     [U1, D1, V1] = svd(F);   
     D1(3, 3) = 0;
-
+    
     F_tilde = U1*D1*V1';
 
+    % task 1.2.3 - de-normalization
     F = T2'*F_tilde*T1;
 end
--- assignment3/main.m
+++ assignment3/main.m
@@ -3,14 +3,15 @@ clear, clc;
 close all;
 
 addpath('include')
+addpath('media')
 
 % Load images
-img1 = imread('media/Rubik1.pgm');
-img2 = imread('media/Rubik2.pgm');
+img1 = imread('Rubik1.pgm');
+img2 = imread('Rubik2.pgm');
 
 % Load points
-P1orig = load('media/Rubik1.points'); % [13r, 2c]
-P2orig = load('media/Rubik2.points');
+P1orig = load('Rubik1.points'); % [13r, 2c]
+P2orig = load('Rubik2.points');
 
 %P1orig = P1orig(1:10,:);
 %P2orig = P2orig(1:10,:);
@@ -50,6 +51,7 @@ end
 
 % ---- w/out normalization ---- 
 F = EightPointsAlgorithm(P1, P2);
+figure
 visualizeEpipolarLines(img1, img2, F, P1orig, P2orig, 1);
 
 % ---- w/ normalization ----
@@ -58,6 +60,7 @@ figure
 visualizeEpipolarLines(img1, img2, Fn, P1orig, P2orig, 2);
 
 
+% task 2.1 - epipolar constraint check
 % ---- evaluation ----
 z = []
 for i = 1:n
@@ -72,16 +75,17 @@ sum(z<TH)
 % % close all
 % visualizeEpipolarLines(img1, img2, F, [], [], 110);
 %% Part 1: w/ Mire
-clc;
-close all;
+clc; clear;
+addpath('include')
+addpath('media')
 
 % Load images
-img1 = imread('media/Mire1.pgm');
-img2 = imread('media/Mire2.pgm');
+img1 = imread('Mire1.pgm');
+img2 = imread('Mire2.pgm');
 
 % Load points
-P1orig = load('media/Mire1.points');
-P2orig = load('media/Mire2.points');
+P1orig = load('Mire1.points');
+P2orig = load('Mire2.points');
 
 n = size(P1orig,1);
 
@@ -91,6 +95,7 @@ P2 = [P2orig'; ones(1,n)];
 
 % ---- w/out normalization ---- 
 F = EightPointsAlgorithm(P1, P2);
+figure
 visualizeEpipolarLines(img1, img2, F, P1orig, P2orig, 1);
 
 % ---- w/ normalization ----
@@ -101,14 +106,16 @@ visualizeEpipolarLines(img1, img2, Fn, P1orig, P2orig, 2);
 
 %% Part 2: assessing the use of RANSAC 
 clc, clear all;
+addpath('include')
+addpath('media')
 
 % Load images
-img1 = imread('Rubik/Rubik1.pgm');
-img2 = imread('Rubik/Rubik2.pgm');
+img1 = imread('Rubik1.pgm');
+img2 = imread('Rubik2.pgm');
 
 % Load points
-P1orig = load('Rubik/Rubik1.points');
-P2orig = load('Rubik/Rubik2.points');
+P1orig = load('Rubik1.points');
+P2orig = load('Rubik2.points');
 
 % Add random points (to assess RANSAC)
 x1r = double(round(size(img1,1)*rand(5,1)));
@@ -135,16 +142,14 @@ th = 10^(-2);
 % Visualize the epipolar lines
 visualizeEpipolarLines(img1, img2, F, P1orig, P2orig, 120);
 
-
 %% Part 3: using image matching+ransac
 clc, close all, clear all;
-% -------------------------------------------------------------------------
-addpath('../../ImageMatching/'); % fix the path to include last lecture functions
-% -------------------------------------------------------------------------
+addpath('include')
+addpath('media')
 
 % Load images
-img1 = rgb2gray(imread('OtherPairs/Img6.jpeg'));
-img2 = rgb2gray(imread('OtherPairs/Img5.jpeg'));
+img1 = rgb2gray(imread('Img6.jpeg'));
+img2 = rgb2gray(imread('Img5.jpeg'));
 
 img1 = imresize(img1, 0.5);
 img2 = imresize(img2, 0.5);
