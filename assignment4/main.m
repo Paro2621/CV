287a2f5b264130846f37e6ac1f84ddecc58ff061
Pedemontemarina <pedemari.rr@gmail.com>
2025-12-16 15:57:00 +0100

    relative path of videos!!

--- assignment4/main.m
+++ assignment4/main.m
@@ -4,5 +4,5 @@ clear;
 addpath('videos'); % put relevant functions inside the /include folder 
 
 %% threshold too low
-video="C:\Users\pedem\Desktop\computer vision\Assignments\CV\assignment4\videos\luce_vp_1part.mp4";
+video="C:\Users\pedem\Desktop\computer vision\Assignments\videos\luce_vp_1part.mp4";
 compareCDAlgo(video,10)
