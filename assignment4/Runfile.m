4be96143a885ca7830b24f86f2eedcbc8297e045
Pedemontemarina <pedemari.rr@gmail.com>
2026-01-06 16:33:37 +0100

    minor updates

--- assignment4/Runfile.m
+++ assignment4/Runfile.m
@@ -5,6 +5,6 @@ clear;
 addpath('videos\');
 video3="DibrisHall_3part.mp4";
 video2="tennis_2part.mp4";
-%segmantAndTrack(video3,20,0.2,15)
+segmantAndTrack(video3,15,0.5,10)
 %compareCDAlgo(video3,20,0.02,5)
-compareCDOF(video2,35,0.2,20,8)
+% compareCDOF(video2,35,0.2,20,8)
